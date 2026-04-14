6b873045d34e29605c594ad10436fdf149a66864
unknown <rafisiregar1721@gmail.com>
2026-04-14 15:28:46 +0700

    setup database

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Support\Facades\Route; 
+ 
+// Include Student Controller 
+use App\Http\Controllers\StudentController; 
+ 
+Route::get('/', function () { 
+    return redirect('/student'); 
+}); 
+ 
+Route::get('/student', [StudentController::class, 'index']) 
+    ->name('student.index'); 
+ 
+Route::get('/student/add', [StudentController::class, 'create']) 
+    ->name('student.create'); 
+Route::POST('/student/add', [StudentController::class, 'store']) 
+    ->name('student.store'); 
+ 
+Route::get('/student/{id}', [StudentController::class, 'show']) 
+    ->name('student.show'); 
+ 
+Route::get('/student/edit/{id}', [StudentController::class, 'edit']) 
+    ->name('student.edit'); 
+Route::PUT('/student/edit/{id}', [StudentController::class, 'update']) 
+    ->name('student.update'); 
+ 
+Route::DELETE('/student/delete/{id}', [StudentController::class, 'destroy']) 
+    ->name('student.destroy');
